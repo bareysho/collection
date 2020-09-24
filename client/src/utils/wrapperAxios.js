57df28e69ba559a6357ee4674c4d2b2c01adea46
Artur_Bareisho <bareysho@gmail.com>
2020-09-24 12:57:31 +0300

    kek

--- client/src/utils/wrapperAxios.js
+++ client/src/utils/wrapperAxios.js
@@ -5,14 +5,14 @@ export default class WrapperAxios {
         console.log(api);
         console.log(url);
         //return axios.post(`http://localhost:8080/${url}`, data, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } });
-        return axios.post(`${api}/${url}`, data, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } });
+        return axios.post(`${url}`, data, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } });
     }
 
     put(url, data) {
         console.log(api);
         console.log(url);
         //return axios.put(`http://localhost:8080/${url}`, data, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } });
-        return axios.put(`${api}/${url}`, data, { headers: {Authorization: 'Bearer ' + localStorage.getItem('token') } })
+        return axios.put(`${url}`, data, { headers: {Authorization: 'Bearer ' + localStorage.getItem('token') } })
 
     }
 
@@ -20,11 +20,11 @@ export default class WrapperAxios {
         console.log(api);
         console.log(url);
         //return axios.get(`http://localhost:8080/${url}`, { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } });
-        return axios.get(`${api}/${url}`,  { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
+        return axios.get(`${url}`,  { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
     }
 
     delete(url, data) {
-        return axios.delete(`${api}/${url}`, {data : {data},  headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } });
+        return axios.delete(`${url}`, {data : {data},  headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } });
         //return axios.get(`${api}/${url}`,  { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } })
     }
 
